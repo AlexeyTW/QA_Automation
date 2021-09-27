4335271b65e07919b0a4369cf2b3837b54bde551
Alexey Barinov <alexey.barinov@transwareag.com>
2021-09-27 17:55:15 +0200

    New Python files have been added

--- QA_Tests.py
+++ QA_Tests.py
@@ -1,4 +1,5 @@
-import time, math, os
+import time, math, os, unittest
+
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -8,16 +9,30 @@ from selenium.webdriver.support import expected_conditions as EC
 def calc(x):
 	return str(math.log(abs(12*math.sin(int(x)))))
 
-browser = webdriver.Chrome()
-browser.implicitly_wait(5)
-try:
-	browser.get("http://suninjuly.github.io/explicit_wait2.html")
-	WebDriverWait(browser, 12).until(EC.text_to_be_present_in_element((By.ID, 'price'), '100'))
-	browser.find_element_by_id('book').click()
-	input_value = browser.find_element_by_id('input_value').text
-	browser.find_element_by_id('answer').send_keys(calc(input_value))
-	browser.find_element_by_id('solve').click()
-	answer = browser.switch_to.alert.text
-	print(answer)
-finally:
-	browser.quit()
+class TestCases(unittest.TestCase):
+	def test1(self):
+		try:
+			browser = webdriver.Chrome()
+			browser.implicitly_wait(5)
+			browser.get('https://suninjuly.github.io/registration1.html')
+			browser.find_element_by_xpath('//input[contains(@placeholder, "first name")]').send_keys('First name')
+			browser.find_element_by_xpath('//input[contains(@placeholder, "last name")]').send_keys('Second name')
+			browser.find_element_by_xpath('//input[contains(@placeholder, "email")]').send_keys('email@test.com')
+			browser.find_element_by_css_selector("[type='submit']").click()
+			WebDriverWait(browser, 5).until(EC.url_contains('result'))
+			self.assertEqual(browser.find_element(By.TAG_NAME, 'h1').text, 'Congratulations! You have successfully registered!', 'You are not registered')
+		finally:
+			browser.quit()
+	def test2(self):
+		try:
+			browser = webdriver.Chrome()
+			browser.implicitly_wait(5)
+			browser.get('http://suninjuly.github.io/registration2.html')
+			browser.find_element_by_xpath('//input[contains(@placeholder, "your name")]').send_keys('First name')
+			browser.find_element_by_xpath('//input[contains(@placeholder, "first name")]').send_keys('Second name')
+			browser.find_element_by_xpath('//input[contains(@placeholder, "email")]').send_keys('email@test.com')
+			browser.find_element_by_css_selector("[type='submit']").click()
+			WebDriverWait(browser, 5).until(EC.url_contains('result'))
+			self.assertEqual(browser.find_element(By.TAG_NAME, 'h1').text, 'Congratulations! You have successfully registered!', 'You are not registered')
+		finally:
+			browser.quit()
