418c3fde5b9ec71c2c464173c3946879146371a1
Alexey Barinov <alexey.barinov@transwareag.com>
2021-10-01 12:45:54 +0200

    Repository has been refactored to be in "empty" state

---
+++ _Archive/temp.py
@@ -0,0 +1,67 @@
+class Figure:
+    def __init__(self, color):
+        self.__color = color
+
+    @property
+    def color(self):
+        return self.__color
+
+    @color.setter
+    def color(self, color):
+        self.__color = color
+
+
+class rectangle(Figure):
+
+    def __init__(self, width, height, color):
+        super().__init__(color)
+        self.__width = width
+        self.__height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @property
+    def height(self):
+        return self.__height
+
+    @width.setter
+    def width(self, w):
+        if w > 0:
+            self.__width = w
+        else:
+            raise ValueError
+
+    @height.setter
+    def height(self, h):
+        if h > 0:
+            self.__height = h
+        else:
+            raise ValueError
+
+    def area(self):
+        return self.__width * self.__height
+
+class TestClass:
+    def __init__(self, line):
+        self.line = line
+
+    @property
+    def line_to_reverse(self):
+        return self.line
+
+    @line_to_reverse.setter
+    def line_to_reverse(self, l):
+        self.line = l
+
+    def rev_line(self):
+        return ''.join([i for i in reversed(self.line)])
+
+
+s = TestClass('test')
+
+s.line_to_reverse = 'erty'
+
+
+print(s.rev_line())
