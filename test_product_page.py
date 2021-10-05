5a1a883946cea23f14086ab91a0e170974ee8558
Alexey Barinov <barinov_a@inbox.ru>
2021-10-06 00:19:35 +0300

    Product page with parameters is done

--- test_product_page.py
+++ test_product_page.py
@@ -3,9 +3,13 @@ from .pages.main_page import MainPage
 from .pages.product_page import ProductPage, ProductPageLocators, PromoPagesLocators
 import time, pytest
 
-@pytest.mark.skip
-def test_guest_can_add_product_to_basket(browser):
-    page = ProductPage(browser, ProductPageLocators.PRODUCT_LINK)
+
+@pytest.mark.parametrize('promo_num', [0, 1, 2, 3, 4, 5, 6,
+                                       pytest.param('bugged promo number', marks=pytest.mark.xfail),
+                                       8, 9])
+def test_guest_can_add_product_to_basket(browser, promo_num):
+    link = PromoPagesLocators.PROMO_LINK + str(promo_num)
+    page = ProductPage(browser, link)
     page.open()
     product_name = page.get_product_name()
     product_price = page.get_product_price()
@@ -15,12 +19,3 @@ def test_guest_can_add_product_to_basket(browser):
     assert product_name == added_product_name
     basket_price = page.get_price_of_basket()
     assert product_price == basket_price
-
-
-@pytest.mark.promo
-@pytest.mark.parametrize('promo_num', range(10))
-def test_promo_pages(browser, promo_num):
-    link = PromoPagesLocators.PROMO_LINK + str(promo_num)
-    page = ProductPage(browser, link)
-    page.open()
-    time.sleep(1)
